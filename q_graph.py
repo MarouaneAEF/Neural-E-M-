65af976a389290a0b64ad05a45c961ed6c34feec
AIT EL FAQIR Marouane <marouane.ait-el-faqir@csgroup.eu>
2023-04-26 16:42:41 +0200

    src: main loop version init

--- q_graph.py
+++ q_graph.py
@@ -3,15 +3,15 @@ import tensorflow as tf
 from tensorflow.keras import layers
 
 
-class q_graph(tf.keras.Model):
+class Q_graph(tf.keras.Model):
 
     def __init__(self):
-        super(q_graph, self).__init__()
+        super(Q_graph, self).__init__()
         
         self.bloc_encoder = tf.keras.Sequential(
-            
-        [  layers.LayerNormalization(),  
-           layers.Reshape((None, 24, 24, 1)),
+        [  
+            layers.LayerNormalization(),  
+            layers.Reshape((None, 24, 24, 1)),
             layers.Conv2D(
                 filters=32, kernel_size=4, strides=(2, 2), activation='relu'),
             layers.Conv2D(
@@ -20,10 +20,9 @@ class q_graph(tf.keras.Model):
                 filters=128, kernel_size=4, strides=(2, 2), activation='relu'),
             layers.Flatten(),
             layers.Dense(512),
-            layers.Reshape(target_shape = (512,1))
-        ]
+            layers.Reshape(target_shape = (512,1)),
 
-        )
+            ])
 
         self.rnn = layers.SimpleRNN(250, activation="sigmoid", return_state=True)
 
@@ -48,11 +47,12 @@ class q_graph(tf.keras.Model):
                                                 padding='same',
                                                 activation='relu'),
                 layers.Flatten(),
+                
+                ])
 
-            ]
-        )
+
     def call(self, inputs, theta):
-        x = self.bloc_encoder(x)
+        x = self.bloc_encoder(inputs)
         x, theta = self.rnn(x, initial_state=theta)
         x = self.decoder_bloc(x)
 
